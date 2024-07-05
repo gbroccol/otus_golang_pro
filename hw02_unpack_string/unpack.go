c76dde8dd8b33ad9a1041038738916c8f5b4c1d6
pchelina.a2 <pchelina.a2@wildberries.work>
2024-07-05 13:14:24 +0200

    hw-02

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -21,6 +21,13 @@ func Unpack(str string) (string, error) {
 			return "", ErrInvalidString
 		}
 
+		if letter == 92 && i+1 < len(str) {
+			i++
+			letter = str[i]
+		} else if letter == 92 {
+			return "", ErrInvalidString
+		}
+
 		if i+1 < len(str) {
 			digit := rune(str[i+1])
 			if unicode.IsDigit(digit) {
