9034dade25da83120425859978063551ececafdb
pchelina.a2 <pchelina.a2@wildberries.work>
2024-07-05 12:56:46 +0200

    hw-02 (part 1)

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,39 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+
+	var result strings.Builder
+	i := 0
+
+	for i < len(str) {
+
+		letter := str[i]
+
+		if unicode.IsDigit(rune(letter)) {
+			return "", ErrInvalidString
+		}
+
+		if i+1 < len(str) {
+			digit := rune(str[i+1])
+			if unicode.IsDigit(digit) {
+				for amount := int(digit - '0'); amount > 0; amount-- {
+					result.WriteByte(letter)
+				}
+				i++
+			} else {
+				result.WriteByte(letter)
+			}
+		} else {
+			result.WriteByte(letter)
+		}
+		i++
+	}
+	return result.String(), nil
 }
