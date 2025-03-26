087cde3ff19f90d179f4b271c093df3ad6f7bac6
pchelina.a2 <pchelina.a2@wildberries.work>
2025-03-26 08:44:56 +0300

    hw-01

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/agrison/go-commons-lang/stringUtils"
 )
 
+func reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
 func main() {
-	fmt.Println(stringUtils.Reverse("Hello, OTUS!"))
+	fmt.Println(reverse("Hello, OTUS!"))
 }
