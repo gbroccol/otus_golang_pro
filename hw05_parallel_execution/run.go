476954e22b64eecd6fd3df226422e1b288ac03f1
pchelina.a2 <pchelina.a2@wildberries.work>
2025-05-15 21:36:30 +0200

    hw05 parallel execution tasks

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -1,15 +1,71 @@
 package hw05parallelexecution
 
 import (
+	"context"
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	if n <= 0 {
+		n = 1
+	}
+
+	tasksCh := make(chan Task)                              // канал с задачами, из него воркеры будут брать задачи
+	ctx, cancel := context.WithCancel(context.Background()) // позволяет остановить все горутины по команде
+	defer cancel()                                          // гарантируем отмену
+
+	var wg sync.WaitGroup
+
+	var errCountMutex sync.Mutex // Защищает общий счётчик ошибок errCount
+	var errCount int
+
+	// запускаем N воркеров
+	for i := 0; i < n; i++ {
+		wg.Add(1) // в sync.WaitGroup запоминаем кол-во работающих потоков
+		go func() {
+			defer wg.Done()
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case task, ok := <-tasksCh:
+					if !ok {
+						return
+					}
+					if err := task(); err != nil {
+						errCountMutex.Lock()
+						errCount++
+						if m > 0 && errCount >= m { //
+							cancel()
+						}
+						errCountMutex.Unlock()
+					}
+				}
+			}
+		}()
+	}
+
+	// producer - заполняет канал задачами
+	go func() {
+		defer close(tasksCh)
+		for _, task := range tasks {
+			select {
+			case <-ctx.Done():
+				return
+			case tasksCh <- task:
+			}
+		}
+	}()
+
+	wg.Wait()
+
+	if m > 0 && errCount >= m {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
 }
