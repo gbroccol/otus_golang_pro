1052a2f111a7d380b874ef7917c41520e1a545ca
pchelina.a2 <pchelina.a2@wildberries.work>
2025-05-21 23:08:43 +0200

    fix

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -15,42 +15,10 @@ func Run(tasks []Task, n, m int) error {
 		n = 1
 	}
 
-	tasksCh := make(chan Task)                              // канал с задачами, из него воркеры будут брать задачи
-	ctx, cancel := context.WithCancel(context.Background()) // позволяет остановить все горутины по команде
-	defer cancel()                                          // гарантируем отмену
+	tasksCh := make(chan Task)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	var wg sync.WaitGroup
-
-	var errCountMutex sync.Mutex // Защищает общий счётчик ошибок errCount
-	var errCount int
-
-	// запускаем N воркеров
-	for i := 0; i < n; i++ {
-		wg.Add(1) // в sync.WaitGroup запоминаем кол-во работающих потоков
-		go func() {
-			defer wg.Done()
-			for {
-				select {
-				case <-ctx.Done():
-					return
-				case task, ok := <-tasksCh:
-					if !ok {
-						return
-					}
-					if err := task(); err != nil {
-						errCountMutex.Lock()
-						errCount++
-						if m > 0 && errCount >= m { //
-							cancel()
-						}
-						errCountMutex.Unlock()
-					}
-				}
-			}
-		}()
-	}
-
-	// producer - заполняет канал задачами
 	go func() {
 		defer close(tasksCh)
 		for _, task := range tasks {
@@ -62,10 +30,44 @@ func Run(tasks []Task, n, m int) error {
 		}
 	}()
 
-	wg.Wait()
+	errCount := startWorkers(ctx, n, m, tasksCh, cancel)
 
 	if m > 0 && errCount >= m {
 		return ErrErrorsLimitExceeded
 	}
 	return nil
+}
+
+func startWorkers(ctx context.Context, n, m int, tasksCh <-chan Task, cancel context.CancelFunc) int {
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	errCount := 0
+
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case task, ok := <-tasksCh:
+					if !ok {
+						return
+					}
+					if err := task(); err != nil {
+						mu.Lock()
+						errCount++
+						if m > 0 && errCount >= m {
+							cancel()
+						}
+						mu.Unlock()
+					}
+				}
+			}
+		}()
+	}
+
+	wg.Wait()
+	return errCount
 }
